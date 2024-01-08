297b8edfca281992b059a3a1fdf12051f4fdd690
diegojmoir <diegojmoir@gmail.com>
2024-01-07 20:59:08 -0800

    feat: add background color on caption to make the title visible

--- apps/metaphoto/src/components/Card.tsx
+++ apps/metaphoto/src/components/Card.tsx
@@ -22,8 +22,8 @@ export const Card = ({ photo }: CardProps) => {
                 src={photo.thumbnailUrl}
                 alt={photo.title}
             />
-            <figcaption className="absolute w-full bottom-2 transition duration-300 ease-in-out group-hover:-translate-y-1 group-hover:translate-x-3 group-hover:scale-110">
-                <h2 className="p-2 m-0 text-md  font-bold truncate">
+            <figcaption className="absolute w-full bottom-0 bg-slate-900 transition duration-300 ease-in-out group-hover:-translate-y-1 group-hover:translate-x-3 group-hover:scale-110">
+                <h2 className="p-2 m-0 text-md font-bold truncate text-white">
                     {photo.title}
                 </h2>
             </figcaption>
