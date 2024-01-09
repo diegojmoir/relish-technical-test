4293e0a7bfdab3287a2377ff8c042199c2aa08a0
Diego <diegojmoir@gmail.com>
2024-01-08 19:27:38 -0800

    fix: remove get-photos folder

--- endpoints/get-photos/src/env.ts
+++
@@ -1,8 +0,0 @@
-const config =
-    process.env.NODE_ENV !== 'production' ? await import('dotenv') : null;
-
-if (config) config.config();
-
-export const PORT = process.env.PORT || 5000;
-
-export const BASE_URL = 'https://jsonplaceholder.typicode.com';
--- endpoints/get-photos/src/utils/constants.ts
+++
@@ -1,2 +0,0 @@
-export const DEFAULT_PAGE_SIZE = 25;
-export const DEFAULT_PAGE_OFFSET = 0;
